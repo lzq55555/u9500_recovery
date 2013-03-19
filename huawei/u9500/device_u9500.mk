982c8c8213cdea9a9461287adce24e135a9124c3
lzq55555 <15168341@qq.com>
2013-03-19 13:54:07 +0800

    TWRP

--- huawei/u9500/device_u9500.mk
+++ huawei/u9500/device_u9500.mk
@@ -22,3 +22,8 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_u9500
 PRODUCT_DEVICE := u9500
+
+# Files needed for recovery image
+PRODUCT_COPY_FILES += \
+  device/huawei/U9500/recovery/sbin/chargeservice:/recovery/root/sbin/chargeservice \
+  device/huawei/U9500/recovery/720x1280/:/recovery/root/res/images/720x1280/
